7e3870c0cc1955a792b6605e24b69d422cad29aa
Edward Kim <edward@edykim.com>
2023-11-01 19:52:22 -0700

    [DS3 #42] Add sample tests

---
+++ test/ui/SampleTest.java
@@ -0,0 +1,15 @@
+package ui;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @TODO Remove this test at some point later
+ */
+public class SampleTest {
+  @Test
+  public void runSampleTest() {
+    assertTrue(true);
+  }
+}
