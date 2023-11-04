d2026a8e05870e2c96ac1ea2faead919a9b45192
osenmukdav <47229463+osenmukdav@users.noreply.github.com>
2023-11-04 00:37:55 -0700

    Start of client-server support

--- src/client/Controller.java
+++ src/client/Controller.java
@@ -5,21 +5,31 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.util.LinkedList;
+import java.util.List;
 
 public class Controller {
     private enum State {
         HOME, RECORDING, EXPANDED, EDIT, DELETE, NEW
     }
 
-    LinkedList<Recipe> recipes = new LinkedList<Recipe>();
+    LinkedList<Recipe> recipes;
     private State state;
     private Stage primaryStage;
     private Model model;
 
+    //TODO: This is a stub to show basic idea of the recipe list being set up, the full version should work with multiple recipes
+    private void ListSetup() {
+        String res = model.performRequest("GET", null);
+        String[] result = res.split("|");
+        recipes.add(new Recipe(result[0], result[1]));
+    }
+
     public Controller(Stage primaryStage) {
+        recipes = new LinkedList<Recipe>();
         this.primaryStage = primaryStage;
         this.model = new Model();
         this.state = State.HOME;
+        ListSetup();
         primaryStage.setScene(new Scene(new HomeScene(recipes)));
     }
 
