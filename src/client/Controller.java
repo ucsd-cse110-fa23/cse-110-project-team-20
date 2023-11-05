9f29dd64e75bf7e41cebfc299f6c8a7cf16ae042
osenmukdav <47229463+osenmukdav@users.noreply.github.com>
2023-11-04 21:46:56 -0700

    Actually change to US6

--- src/client/Controller.java
+++ src/client/Controller.java
@@ -1,37 +1,147 @@
 package client;
 
-import client.SceneFiles.HomeScene;
+import client.components.AnimatedLoadingBar;
+import client.components.HomePage;
+import client.components.RecordingPage;
+import java.util.ArrayList;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.util.LinkedList;
-import java.util.List;
-
 public class Controller {
     private enum State {
-        HOME, RECORDING, EXPANDED, EDIT, DELETE, NEW
+        HOME,
+        RECORDING_MEAL_TYPE,
+        RECORDING_INGREDIENTS,
+        LOADING_RECIPE,
+        EXPANDED,
+        EDIT,
+        DELETE,
+        NEW
     }
+    private final String MEAL_TYPE_AUDIO = "meal_type.wav";
+    private final String INGREDIENTS_AUDIO = "ingredients.wav";
 
-    LinkedList<Recipe> recipes;
+    ArrayList<Recipe> recipes = new ArrayList<Recipe>();
     private State state;
     private Stage primaryStage;
     private Model model;
+    private Scene recordMealType, recordIngredients, loading, home;
+    private RecordingPage mealTypePage, ingredientsPage;
+    private HomePage homePage;
+    private AnimatedLoadingBar loadingPage;
+    private static final int WIDTH = 500, HEIGHT = 500;
 
-    //TODO: This is a stub to show basic idea of the recipe list being set up, the full version should work with multiple recipes
-    private void ListSetup() {
-        String res = model.performRequest("GET", null);
-        String[] result = res.split("|");
-        recipes.add(new Recipe(result[0], result[1]));
-    }
+    private String mealTypeTranscription, ingredientsTranscription;
 
-    public Controller(Stage primaryStage) {
-        recipes = new LinkedList<Recipe>();
+    private boolean recording;
+
+    public Controller(Stage primaryStage)
+    {
         this.primaryStage = primaryStage;
         this.model = new Model();
+        this.recording = false;
         this.state = State.HOME;
-        ListSetup();
-        primaryStage.setScene(new Scene(new HomeScene(recipes)));
+
+        mealTypePage = new RecordingPage(
+                "What kind of meal do you want?\nLunch, Dinner, Snack etc.", MEAL_TYPE_AUDIO);
+        mealTypePage.setButtonCallback(() -> mealTypeRecordingButtonClicked());
+        this.recordMealType = new Scene(mealTypePage, WIDTH, HEIGHT);
+
+        ingredientsPage = new RecordingPage("What ingredients do you have?", INGREDIENTS_AUDIO);
+        ingredientsPage.setButtonCallback(() -> ingredientsRecordingButtonClicked());
+        this.recordIngredients = new Scene(ingredientsPage, WIDTH, HEIGHT);
+
+        loadingPage = new AnimatedLoadingBar();
+        loadingPage.setLoadingText("Finding the perfect recipe...");
+        this.loading = new Scene(loadingPage, WIDTH, HEIGHT);
+
+        homePage = new HomePage(new ArrayList());
+        homePage.setCreateButtonCallback(() -> createRecipeButtonClicked());
+        this.home = new Scene(homePage, WIDTH, HEIGHT);
+        // Set the title of the app
+        primaryStage.setTitle("PantryPal");
+        // Create scene of mentioned size with the border pane
+        primaryStage.setScene(this.home);
+        // Make window non-resizable
+        primaryStage.setResizable(false);
     }
 
-    //TODO: Add methods for making requests through Model, and add button actions when adding the scenes
+    public void
+    start()
+    {
+        // Show the app
+        primaryStage.show();
+    }
+
+    public void
+    transitionToMealTypeScene()
+    {
+        this.state = State.RECORDING_MEAL_TYPE;
+        primaryStage.setScene(recordMealType);
+    }
+
+    public void
+    transitionToIngredientsScene()
+    {
+        this.state = State.RECORDING_INGREDIENTS;
+        primaryStage.setScene(recordIngredients);
+    }
+
+    public void
+    requestMealTypeTranscription()
+    {
+        // TODO: send API request through model
+        // this.model.requestTranscription(audioFilePath);
+    }
+
+    public void
+    requestIngredientsTranscription()
+    {
+        // TODO: send API request through model
+        // this.model.requestTranscription(audioFilePath);
+    }
+
+    public void
+    transitionToLoadingScene()
+    {
+        this.state = State.LOADING_RECIPE;
+        primaryStage.setScene(loading);
+    }
+
+    public void
+    ingredientsRecordingButtonClicked()
+    {
+        if (recording) {
+            // Called after second click
+            this.ingredientsPage.stopRecording();
+            this.requestIngredientsTranscription();
+            this.transitionToLoadingScene();
+        } else {
+            this.ingredientsPage.startRecording();
+        }
+        recording = !recording;
+    }
+
+    public void
+    mealTypeRecordingButtonClicked()
+    {
+        if (recording) {
+            // Called after second click
+            this.mealTypePage.stopRecording();
+            this.requestMealTypeTranscription();
+            this.transitionToIngredientsScene();
+        } else {
+            this.mealTypePage.startRecording();
+        }
+        recording = !recording;
+    }
+
+    public void
+    createRecipeButtonClicked()
+    {
+        this.transitionToMealTypeScene();
+    }
+
+    // TODO: Add methods for making requests through Model, and add button actions when adding the
+    // scenes
 }
