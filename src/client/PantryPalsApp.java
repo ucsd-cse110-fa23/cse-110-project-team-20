da8606eb38e70a184a65a7b7aa7dcd0bf9dd1806
Edward Kim <edward@edykim.com>
2023-11-06 09:08:07 -0800

    [DS4-4 #67] Update story runner

--- src/client/PantryPalsApp.java
+++ src/client/PantryPalsApp.java
@@ -12,28 +12,12 @@ import javafx.stage.Stage;
  * PantryPals JavaFX Application
  */
 public class PantryPalsApp extends Application {
-    private static AppInspection inspector;
-
-    public static void
-    main(String[] args)
-    {
-        launch(args);
-    }
-
-    public static void
-    inspect(AppInspection thisInspector)
-    {
-        inspect(thisInspector, null);
-    }
-
-    public static void
-    inspect(AppInspection thisInspector, String[] args)
-    {
-        inspector = thisInspector;
+    public static void main(String[] args) {
         launch(args);
     }
 
     @Override
+
     public void
     start(Stage primaryStage) throws Exception
     {
@@ -45,9 +29,44 @@ public class PantryPalsApp extends Application {
         Controller controller = new Controller(primaryStage, generateRecipe);
         controller.start();
 
+        // debug purposes
+        handleInspect(primaryStage, controller);
+    }
+
+    private static PantryPalsApp _app;
+    private static Controller _controller;
+    private static Stage _primeStage;
+    private static AppInspection inspector;
+
+    private void handleInspect(Stage primaryStage, Controller controller) throws Exception {
+        _app = this;
+        _controller = controller;
+        _primeStage = primaryStage;
         if (inspector != null) {
             inspector.inspect(this, primaryStage, controller);
-            Platform.exit();
         }
     }
+
+    public static Thread inspect(AppInspection thisInspector) {
+        return inspect(thisInspector, null);
+    }
+
+    public static Thread inspect(AppInspection thisInspector, String[] args) {
+        inspector = thisInspector;
+        Thread appRunner = new Thread(() -> {
+            if (_app == null) {
+                launch(args);
+            } else {
+                Platform.runLater(() -> {
+                    try {
+                        inspector.inspect(_app, _primeStage, _controller);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                });
+            }
+        });
+        return appRunner;
+    }
+
 }
