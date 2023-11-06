145837832e760304d7c7a56b673d25241cdfca82
Edward Kim <edward@edykim.com>
2023-11-06 09:31:24 -0800

    [DS4-4 #67] Adjust delay speed for github actions

--- test/feature/UserStoryTestBase.java
+++ test/feature/UserStoryTestBase.java
@@ -2,19 +2,35 @@ package feature;
 
 import static org.junit.jupiter.api.Assertions.fail;
 
+import org.junit.jupiter.api.BeforeAll;
+
 import client.Controller;
 import client.PantryPalsApp;
 import javafx.application.Platform;
 import javafx.stage.Stage;
 
 abstract public class UserStoryTestBase {
+  private static Double delayMultifactor;
+
+  @BeforeAll
+  public static void setUpDelay()
+  {
+    String value = System.getProperty("test_delay_factor");
+
+    try {
+      delayMultifactor = Double.parseDouble(value);
+   } catch(NumberFormatException ex) {
+      delayMultifactor = 1.0;
+   }
+  }
+
   // setTimeout helper
   // ref:
   // https://stackoverflow.com/questions/26311470/what-is-the-equivalent-of-javascript-settimeout-in-java
   public static void setTimeout(Runnable runnable, int delay) {
     new Thread(() -> {
       try {
-        Thread.sleep(delay);
+        Thread.sleep(Math.round(delay * delayMultifactor));
         Platform.runLater(runnable);
       } catch (Exception e) {
       }
@@ -31,8 +47,8 @@ abstract public class UserStoryTestBase {
   protected Controller controller;
 
   public void condition(Runnable runnable) {
-    // minimum 1s to avoid switching delay
-    condition(runnable, 5000);
+    // minimum 2s to avoid switching delay
+    condition(runnable, 2000);
   }
 
   public void condition(Runnable runnable, long utillMillis) {
@@ -46,7 +62,7 @@ abstract public class UserStoryTestBase {
     inspectionThread.start();
 
     try {
-      Thread.sleep(utillMillis);
+      Thread.sleep(Math.round(utillMillis * delayMultifactor));
     } catch (InterruptedException e) {
       fail(e.getMessage());
     }
