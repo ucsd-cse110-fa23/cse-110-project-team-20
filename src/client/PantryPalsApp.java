1641f348595ece5f3874c9f3a29ee2d209e1521a
Edward Kim <edward@edykim.com>
2023-11-06 16:52:36 -0800

    [US6 #9] enable generate recipe server one for client

--- src/client/PantryPalsApp.java
+++ src/client/PantryPalsApp.java
@@ -1,6 +1,8 @@
 package client;
 
+import client.recipe.GenerateRecipe;
 import client.recipe.LocalRecipeGenerator;
+import client.recipe.ServerRecipeGenerator;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
@@ -20,11 +22,10 @@ public class PantryPalsApp extends Application {
     public void
     start(Stage primaryStage) throws Exception
     {
-        LocalRecipeGenerator generateRecipe = new LocalRecipeGenerator();
+        // LocalRecipeGenerator generateRecipe = new LocalRecipeGenerator();
         // generateRecipe.setAlwaysFail(true); // if we need to test failing
 
-        // @TODO replace the server one when server is ready
-        // GenerateRecipe generateRecipe = new ServerRecipeGenerator();
+        GenerateRecipe generateRecipe = new ServerRecipeGenerator();
 
         Controller controller = new Controller(primaryStage, generateRecipe);
         controller.start();
