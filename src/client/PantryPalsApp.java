35790de1aa75d040027aee560d114db8906fb2d4
Edward Kim <edward@edykim.com>
2023-11-08 08:15:06 -0800

    [DS] Remove outdated inspection

--- src/client/PantryPalsApp.java
+++ src/client/PantryPalsApp.java
@@ -3,13 +3,10 @@ package client;
 import client.recipe.IGenerateRecipe;
 import client.audio.AudioRecorder;
 import client.audio.IAudioRecorder;
-import client.helpers.AppInspection;
-import client.recipe.LocalRecipeGenerator;
 import client.recipe.ServerRecipeGenerator;
 import client.utils.transitions.CompositeTransitioner;
 import client.utils.transitions.PrintConsoleTransitioner;
 import javafx.application.Application;
-import javafx.application.Platform;
 import javafx.stage.Stage;
 
 /**
@@ -25,16 +22,11 @@ public class PantryPalsApp extends Application {
     public void
     start(Stage primaryStage) throws Exception
     {
-        IGenerateRecipe generateRecipe;
-
-        if (inspector != null) {
-            generateRecipe = new LocalRecipeGenerator();
-        } else {
-            generateRecipe = new ServerRecipeGenerator();
-        }
-
         CompositeTransitioner transitioner = new CompositeTransitioner();
+
+        IGenerateRecipe generateRecipe = new ServerRecipeGenerator();
         IAudioRecorder audioRecorder = new AudioRecorder();
+
         Controller controller = new Controller(transitioner, generateRecipe, audioRecorder);
 
         transitioner
@@ -42,45 +34,5 @@ public class PantryPalsApp extends Application {
             .add(new PrintConsoleTransitioner());
 
         controller.start();
-
-        // debug purposes
-        handleInspect(primaryStage, controller);
     }
-
-    private static PantryPalsApp _app;
-    private static Controller _controller;
-    private static Stage _primeStage;
-    private static AppInspection inspector;
-
-    private void handleInspect(Stage primaryStage, Controller controller) throws Exception {
-        _app = this;
-        _controller = controller;
-        _primeStage = primaryStage;
-        if (inspector != null) {
-            inspector.inspect(this, primaryStage, controller);
-        }
-    }
-
-    public static Thread inspect(AppInspection thisInspector) {
-        return inspect(thisInspector, null);
-    }
-
-    public static Thread inspect(AppInspection thisInspector, String[] args) {
-        inspector = thisInspector;
-        Thread appRunner = new Thread(() -> {
-            if (_app == null) {
-                launch(args);
-            } else {
-                Platform.runLater(() -> {
-                    try {
-                        inspector.inspect(_app, _primeStage, _controller);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                });
-            }
-        });
-        return appRunner;
-    }
-
 }
