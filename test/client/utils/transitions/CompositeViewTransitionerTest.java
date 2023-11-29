b9d7bb030cd2097af8d0516c4af50a66298a6e09
AlmondHalo <cmrice@ucsd.edu>
2023-11-28 16:03:12 -0800

    Updated tests for US 5

--- test/client/utils/transitions/CompositeViewTransitionerTest.java
+++ test/client/utils/transitions/CompositeViewTransitionerTest.java
@@ -18,15 +18,18 @@ public class CompositeViewTransitionerTest {
     MockTransitioner testTransitioner1 = new MockTransitioner(MockCompositeClass.class);
     MockTransitioner testTransitioner2 = new MockTransitioner(MockCompositeClass.class);
     MockTransitioner testTransitioner3 = new MockTransitioner(MockCompositeClass.class);
+    MockTransitioner testTransitioner4 = new MockTransitioner(MockCompositeClass.class);
 
     transitioner.add(testTransitioner1);
     transitioner.add(testTransitioner2);
     transitioner.add(testTransitioner3);
+    transitioner.add(testTransitioner4);
 
     transitioner.transitionTo(MockCompositeClass.class);
     assertTrue(testTransitioner1.transitionTo0called);
     assertTrue(testTransitioner2.transitionTo0called);
     assertTrue(testTransitioner3.transitionTo0called);
+    assertTrue(testTransitioner4.transitionTo0called);
   }
 
   @Test
@@ -35,15 +38,18 @@ public class CompositeViewTransitionerTest {
     MockTransitioner testTransitioner1 = new MockTransitioner(MockCompositeClass.class, 1);
     MockTransitioner testTransitioner2 = new MockTransitioner(MockCompositeClass.class, 1);
     MockTransitioner testTransitioner3 = new MockTransitioner(MockCompositeClass.class, 1);
+    MockTransitioner testTransitioner4 = new MockTransitioner(MockCompositeClass.class, 1);
 
     transitioner.add(testTransitioner1);
     transitioner.add(testTransitioner2);
     transitioner.add(testTransitioner3);
+    transitioner.add(testTransitioner4);
 
     transitioner.transitionTo(MockCompositeClass.class, 1);
     assertTrue(testTransitioner1.transitionTo1called);
     assertTrue(testTransitioner2.transitionTo1called);
     assertTrue(testTransitioner3.transitionTo1called);
+    assertTrue(testTransitioner4.transitionTo1called);
   }
 
   @Test
@@ -52,15 +58,18 @@ public class CompositeViewTransitionerTest {
     MockTransitioner testTransitioner1 = new MockTransitioner(MockCompositeClass.class, 1, "Some string");
     MockTransitioner testTransitioner2 = new MockTransitioner(MockCompositeClass.class, 1, "Some string");
     MockTransitioner testTransitioner3 = new MockTransitioner(MockCompositeClass.class, 1, "Some string");
+    MockTransitioner testTransitioner4 = new MockTransitioner(MockCompositeClass.class, 1, "Some string");
 
     transitioner.add(testTransitioner1);
     transitioner.add(testTransitioner2);
     transitioner.add(testTransitioner3);
+    transitioner.add(testTransitioner4);
 
     transitioner.transitionTo(MockCompositeClass.class, 1, "Some string");
     assertTrue(testTransitioner1.transitionTo2called);
     assertTrue(testTransitioner2.transitionTo2called);
     assertTrue(testTransitioner3.transitionTo2called);
+    assertTrue(testTransitioner4.transitionTo2called);
   }
 
   @Test
@@ -69,15 +78,38 @@ public class CompositeViewTransitionerTest {
     MockTransitioner testTransitioner1 = new MockTransitioner(MockCompositeClass.class, 1, "Some string", false);
     MockTransitioner testTransitioner2 = new MockTransitioner(MockCompositeClass.class, 1, "Some string", false);
     MockTransitioner testTransitioner3 = new MockTransitioner(MockCompositeClass.class, 1, "Some string", false);
+    MockTransitioner testTransitioner4 = new MockTransitioner(MockCompositeClass.class, 1, "Some string", false);
 
     transitioner.add(testTransitioner1);
     transitioner.add(testTransitioner2);
     transitioner.add(testTransitioner3);
+    transitioner.add(testTransitioner4);
 
     transitioner.transitionTo(MockCompositeClass.class, 1, "Some string", false);
     assertTrue(testTransitioner1.transitionTo3called);
     assertTrue(testTransitioner2.transitionTo3called);
     assertTrue(testTransitioner3.transitionTo3called);
+    assertTrue(testTransitioner4.transitionTo3called);
+  }
+
+  @Test
+  public void cascadeTransitionToWith4Params() {
+    CompositeViewTransitioner transitioner = new CompositeViewTransitioner();
+    MockTransitioner testTransitioner1 = new MockTransitioner(MockCompositeClass.class, 1, "Some string", false, 1);
+    MockTransitioner testTransitioner2 = new MockTransitioner(MockCompositeClass.class, 1, "Some string", false, 1);
+    MockTransitioner testTransitioner3 = new MockTransitioner(MockCompositeClass.class, 1, "Some string", false, 1);
+    MockTransitioner testTransitioner4 = new MockTransitioner(MockCompositeClass.class, 1, "Some string", false, 1);
+
+    transitioner.add(testTransitioner1);
+    transitioner.add(testTransitioner2);
+    transitioner.add(testTransitioner3);
+    transitioner.add(testTransitioner4);
+
+    transitioner.transitionTo(MockCompositeClass.class, 1, "Some string", false, 1);
+    assertTrue(testTransitioner1.transitionTo4called);
+    assertTrue(testTransitioner2.transitionTo4called);
+    assertTrue(testTransitioner3.transitionTo4called);
+    assertTrue(testTransitioner4.transitionTo4called);
   }
 }
 
@@ -89,11 +121,13 @@ class MockTransitioner implements IViewTransitioner {
   public boolean transitionTo1called = false;
   public boolean transitionTo2called = false;
   public boolean transitionTo3called = false;
+  public boolean transitionTo4called = false;
 
   protected Class<?> expectedClass;
   protected Object expected1;
   protected Object expected2;
   protected Object expected3;
+  protected Object expected4;
 
   MockTransitioner(Class<?> cls) {
     expectedClass = cls;
@@ -115,6 +149,14 @@ class MockTransitioner implements IViewTransitioner {
     expected1 = a;
     expected2 = b;
     expected3 = c;
+  }
+
+  <T1, T2, T3, T4> MockTransitioner(Class<?> cls, T1 a, T2 b, T3 c, T4 d) {
+    this(cls);
+    expected1 = a;
+    expected2 = b;
+    expected3 = c;
+    expected4 = d;
   }
 
   @Override
@@ -142,6 +184,12 @@ class MockTransitioner implements IViewTransitioner {
   public <T1, T2, T3> void transitionTo(Class<?> c, T1 t1, T2 t2, T3 t3) {
     if (c == expectedClass && t1 == expected1 && t2 == expected2 && t3 == expected3) {
       transitionTo3called = true;
+    }
+  }
+  @Override
+  public <T1, T2, T3, T4> void transitionTo(Class<?> c, T1 t1, T2 t2, T3 t3, T4 t4) {
+    if (c == expectedClass && t1 == expected1 && t2 == expected2 && t3 == expected3 && t4 == expected4) {
+      transitionTo4called = true;
     }
   }
 }
--- test/feature/mock/MockViewTransitioner.java
+++ test/feature/mock/MockViewTransitioner.java
@@ -10,9 +10,11 @@ public class MockViewTransitioner implements IViewTransitioner {
   public String param1;
   public String param2;
   public String param3;
+  public String param4;
   public Object rawParam1;
   public Object rawParam2;
   public Object rawParam3;
+  public Object rawParam4;
 
   @Override
   public void transitionTo(Class<?> c) {
@@ -56,5 +58,18 @@ public class MockViewTransitioner implements IViewTransitioner {
     rawParam1 = t1;
     rawParam2 = t2;
     rawParam3 = t3;
+  }
+
+  @Override
+  public <T1, T2, T3, T4> void transitionTo(Class<?> c, T1 t1, T2 t2, T3 t3, T4 t4) {
+    currentPageClass = c;
+    param1 = t1.toString();
+    param2 = t2.toString();
+    param3 = t3.toString();
+    param4 = t4.toString();
+    rawParam1 = t1;
+    rawParam2 = t2;
+    rawParam3 = t3;
+    rawParam4 = t4;
   }
 }
