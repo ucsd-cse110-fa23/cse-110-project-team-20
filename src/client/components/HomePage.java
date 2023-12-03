f17385b12ed9131ece7f5aa7038b92c894118f7f
Nathan Thomas <nathanthomas707@gmail.com>
2023-12-03 13:38:49 -0800

    Fix tags when title is too long

--- src/client/components/HomePage.java
+++ src/client/components/HomePage.java
@@ -3,22 +3,22 @@ package client.components;
 import client.Recipe;
 import client.utils.runnables.RunnableWithId;
 import java.util.List;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
-import javafx.scene.layout.Priority;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.scene.control.ComboBox;
 
 /*
  * Home Page
@@ -32,7 +32,8 @@ public class HomePage extends BorderPane {
     private HBox optionsBar;
 
     public HomePage(List<Recipe> recipes, Runnable createButtonCallback,
-            RunnableWithId openRecipeDetailButtonCallback, Runnable logoutButtonCallback) {
+        RunnableWithId openRecipeDetailButtonCallback, Runnable logoutButtonCallback)
+    {
         getStylesheets().add(getClass().getResource("style.css").toExternalForm());
 
         header = new Header();
@@ -63,8 +64,10 @@ class OptionsBar extends HBox {
     private ComboBox<String> filterComboBox;
     private Runnable r;
 
-    public OptionsBar() {
-        ObservableList<String> filterOptions = FXCollections.observableArrayList("Filter: None", "Breakfast", "Lunch", "Dinner");
+    public OptionsBar()
+    {
+        ObservableList<String> filterOptions =
+            FXCollections.observableArrayList("Filter: None", "Breakfast", "Lunch", "Dinner");
 
         filterComboBox = new ComboBox<>(filterOptions);
         filterComboBox.setPromptText("Filter by meal type");
@@ -80,18 +83,20 @@ class OptionsBar extends HBox {
         this.getChildren().addAll(filterComboBox);
     }
 
-    public void setSelectionCallback(Runnable r) {
+    public void
+    setSelectionCallback(Runnable r)
+    {
         this.r = r;
     }
 }
-
 
 class Header extends BorderPane {
     private Label appName;
     private Button createButton;
     private Button logoutButton;
 
-    public Header() {
+    public Header()
+    {
         createButton = new Button("\uff0b"); // unicode version of plus sign
         createButton.getStyleClass().add("create-button");
         createButton.setAlignment(Pos.CENTER);
@@ -112,20 +117,27 @@ class Header extends BorderPane {
         setAlignment(logoutButton, Pos.CENTER_RIGHT);
     }
 
-    public void setCreateButtonCallback(Runnable r) {
+    public void
+    setCreateButtonCallback(Runnable r)
+    {
         this.createButton.setOnAction(e -> r.run());
     }
 
-    public void setLogoutButtonCallback(Runnable r) {
+    public void
+    setLogoutButtonCallback(Runnable r)
+    {
         this.logoutButton.setOnAction(e -> r.run());
     }
 }
 
 class RecipeBox extends HBox {
-    public RecipeBox(Recipe recipe) {
+    private static final int MAX_TITLE_LEN = 38;
+    public RecipeBox(Recipe recipe)
+    {
         VBox recipeDetails = new VBox(10);
 
-        Label titleLabel = new Label(recipe.getTitle());
+        Label titleLabel = new Label(trimTitle(recipe.getTitle()));
+
         titleLabel.getStyleClass().add("recipe-title");
 
         recipeDetails.getChildren().addAll(titleLabel);
@@ -143,35 +155,50 @@ class RecipeBox extends HBox {
         getStyleClass().add("recipe-box");
     }
 
-    private String getMealType(Recipe r) {
+    private static String
+    trimTitle(String title)
+    {
+        String trimmed = title.trim();
+        if (trimmed.length() > MAX_TITLE_LEN) {
+            return title.substring(0, MAX_TITLE_LEN - 3).trim() + "...";
+        }
+        return trimmed;
+    }
+
+    private static String
+    getMealType(Recipe r)
+    {
         switch (r.getMealType().toLowerCase()) {
-            case "breakfast":
-                return "Breakfast";
-            case "lunch":
-                return "Lunch";
-            case "dinner":
-                return "Dinner";
-            default:
-                return "Unknown";
+        case "breakfast":
+            return "Breakfast";
+        case "lunch":
+            return "Lunch";
+        case "dinner":
+            return "Dinner";
+        default:
+            return "Unknown";
         }
     }
 
-    private String getColorHexCode(String mealType) {
-    switch (mealType.toLowerCase()) {
+    private static String
+    getColorHexCode(String mealType)
+    {
+        switch (mealType.toLowerCase()) {
         case "breakfast":
-            return "FAEDCB"; 
+            return "FAEDCB";
         case "lunch":
-            return "C9E4DE"; 
+            return "C9E4DE";
         case "dinner":
-            return "DBCDF0"; 
+            return "DBCDF0";
         default:
-            return "808080"; 
+            return "808080";
+        }
     }
-}
 }
 
 class RecipeList extends VBox {
-    public RecipeList(List<Recipe> recipes, RunnableWithId openRecipeDetailButtonCallback) {
+    public RecipeList(List<Recipe> recipes, RunnableWithId openRecipeDetailButtonCallback)
+    {
         this.setSpacing(10); // sets spacing between contacts
         this.setPrefSize(500, 460);
         getStyleClass().add("recipe-list");
