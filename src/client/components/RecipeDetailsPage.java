0158aacb1b72b7cea7fb0c9c298e1a81adfac631
Nathan Thomas <nathanthomas707@gmail.com>
2023-12-05 15:07:53 -0800

    Improve recipe details UI

--- src/client/components/RecipeDetailsPage.java
+++ src/client/components/RecipeDetailsPage.java
@@ -1,51 +1,50 @@
 package client.components;
 
 import client.Recipe;
+import javafx.geometry.Insets;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.DialogPane;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
-import javafx.scene.control.Alert.AlertType;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import javafx.geometry.Insets;
-import javafx.scene.control.DialogPane;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.paint.Color;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 
 /*
  * Recipe details
  *
- * Handles the structure of the details page that is transitioned to once the user clicks on a recipe on the home page.
- * Contains a cancel, delete, and edit button, along with with their associated callbacks.
+ * Handles the structure of the details page that is transitioned to once the user clicks on a
+ * recipe on the home page. Contains a cancel, delete, and edit button, along with with their
+ * associated callbacks.
  */
 public class RecipeDetailsPage extends BorderPane {
     private RecipeHeader header;
-    private Label body;
+    private VBox body;
     private DetailsFooter footer;
 
     public RecipeDetailsPage()
     {
         // get style sheet
-        getStylesheets().add(getClass().getResource(
-            "style.css"
-        ).toExternalForm());
+        getStylesheets().add(getClass().getResource("style.css").toExternalForm());
 
         // style header
         header = new RecipeHeader();
         header.getStyleClass().add("recipedetails-header");
 
         // style body
-        body = new Label();
-        body.setWrapText(true);
-        body.getStyleClass().add("recipe-body");
+        body = new VBox();
 
         ScrollPane scrollPane = new ScrollPane(body);
         scrollPane.getStyleClass().add("recipe-scroll-pane");
+        scrollPane.setFitToWidth(true);
 
         // style footer
         footer = new DetailsFooter();
@@ -87,13 +86,19 @@ public class RecipeDetailsPage extends BorderPane {
         recipeTitle.setWrapText(true);
         recipeTitle.getStyleClass().add("recipe-title");
         this.header.setCenter(recipeTitle);
-        this.body.setText(recipe.getDescription());
+
+        Label recipeDescription = new Label(recipe.getDescription());
+        recipeDescription.getStyleClass().add("recipe-body");
         String imageurl = recipe.getImageUrl();
         ImageView imageView = new ImageView(new Image(imageurl));
-        imageView.setFitWidth(100); 
-        imageView.setFitHeight(100); 
+        imageView.setFitWidth(200);
+        imageView.setFitHeight(200);
         imageView.getStyleClass().add("recipe-image");
-        this.setLeft(imageView);
+        BorderPane pane = new BorderPane();
+        pane.setPrefSize(500, 200);
+        pane.setCenter(imageView);
+
+        this.body.getChildren().addAll(pane, recipeDescription);
     }
 }
 
@@ -101,14 +106,14 @@ public class RecipeDetailsPage extends BorderPane {
 class RecipeHeader extends BorderPane {
     // private Button cancelButton;
 
-    public RecipeHeader() {
+    public RecipeHeader()
+    {
         getStyleClass().add("recipedetails-header");
         // this.cancelButton = new Button("\u2190");
         // this.cancelButton.getStyleClass().add("cancel-button");
         // setLeft(cancelButton);
         // setAlignment(cancelButton, Pos.TOP_LEFT);
         // cancelButton.setMaxWidth(Region.USE_PREF_SIZE);
-
     }
     // public void
     // setOnCancel(Runnable onCancel)
@@ -116,7 +121,6 @@ class RecipeHeader extends BorderPane {
     //     this.cancelButton.setOnAction(e -> onCancel.run());
     // }
 }
-
 
 class DetailsFooter extends HBox {
     private Button saveButton;
@@ -129,10 +133,10 @@ class DetailsFooter extends HBox {
     {
         this.cancelButton = new Button("\u2190");
         this.cancelButton.getStyleClass().add("cancel-button");
-        
+
         this.editButton = new Button("Edit Recipe");
         this.editButton.getStyleClass().add("edit-button");
-        
+
         this.deleteButton = new Button("Delete Recipe");
         this.deleteButton.getStyleClass().add("delete-button");
 
@@ -155,22 +159,24 @@ class DetailsFooter extends HBox {
     {
         this.editButton.setOnAction(e -> onEdit.run());
     }
-    
+
     public void
     setOnDelete(Runnable onDelete)
     {
         // delete event only triggers when user clicks YES button on the confirmation
         // ref: https://docs.oracle.com/javase/8/javafx/api/javafx/scene/control/Alert.html
         this.deleteButton.setOnAction(e -> {
-            Alert alert = new Alert(AlertType.WARNING, "Do you want to delete this recipe?", ButtonType.CANCEL, ButtonType.YES);
-            
+            Alert alert = new Alert(AlertType.WARNING, "Do you want to delete this recipe?",
+                ButtonType.CANCEL, ButtonType.YES);
+
             // Get the DialogPane
             DialogPane dialogPane = alert.getDialogPane();
 
             // Customize the appearance
-            dialogPane.setBackground(new Background(new BackgroundFill(Color.LIGHTGRAY, null, null)));
+            dialogPane.setBackground(
+                new Background(new BackgroundFill(Color.LIGHTGRAY, null, null)));
             dialogPane.getStyleClass().add("delete-alert");
-            
+
             alert.showAndWait();
 
             if (alert.getResult() == ButtonType.YES) {
